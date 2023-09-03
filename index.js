555cde630750c29badc9ff23956c5b1cee7aa370
Sagar <104849951+kundu03@users.noreply.github.com>
2023-09-04 00:03:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+app.use('/server', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  next();
+});
 // const corsOptions ={
 //     origin:'https://blogbackend-ks9r.onrender.com', 
 //     credentials:true,            //access-control-allow-credentials:true
