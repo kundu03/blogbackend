b4eafe882ccb69375bbbdd3b9f353f6e974942b7
Sagar <104849951+kundu03@users.noreply.github.com>
2023-09-04 19:59:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use('/server', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   next();
 });
